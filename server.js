1c16d5023ef40f8b0734e5efddf9e8550747af34
Zattona <98216918+Zattona@users.noreply.github.com>
2022-12-12 10:02:21 +0200

    adding project

---
+++ server.js
@@ -0,0 +1,83 @@
+//Create a server that can send back static files
+const request = require('request');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+//npm install --save request
+//npm i mime-types
+const lookup = require("mime-types").lookup;
+
+const server = http.createServer((req, res) => {
+  //handle the request and send back a static file
+  //from a folder called `public`
+  let parsedURL = url.parse(req.url, true);
+  //remove the leading and trailing slashes
+  let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
+
+  // get host & protocol
+
+  var host = req.headers.host,
+    subDomain = host.split("."),
+    protocol = req.socket.encrypted ? "https" : "http";
+
+  if (subDomain.length >= 2) {
+    subDomain = subDomain[0].split("-").join(" ");
+  }
+
+  // Get user details by subdomain name [return template name ]
+  request(
+    "https://nodejs.org/api/documentation.json",
+    function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // console.log(body); // Print the google web page.
+        console.log('Get Sample response success');
+      }
+    }
+  );
+
+  console.log("Template Path:", host);
+  console.log("Subdomain:", subDomain);
+  console.log("Subdomain length:", subDomain.length);
+  console.log("Protocol:", protocol);
+  //   console.log(req);
+  /**
+   *  /
+   *  /index.html
+   *
+   *  /main.css
+   *  /main.js
+   */
+  if (path == "") {
+    path = "index.html";
+  }
+  console.log(`Requested path ${path} `);
+
+  let file = __dirname + "/templates/" + subDomain + "/" + path;
+  console.log(__dirname);
+  //async read file function uses callback
+  fs.readFile(file, function (err, content) {
+    if (err) {
+      console.log(`File Not Found ${file}`);
+      res.writeHead(404);
+      res.end();
+    } else {
+      //specify the content type in the response
+      console.log(`Returning ${path}`);
+      res.setHeader("X-Content-Type-Options", "nosniff");
+      //   res.setHeader("USER", subDomain);
+
+      let mime = lookup(path);
+      res.writeHead(200, { "Content-type": mime });
+
+      //   var _repl = "<div>I am new!</div>";
+      //   res.write(_repl);
+
+      res.end(content);
+    }
+  });
+});
+
+server.listen(80, "localhost", () => {
+  console.log("Listening on port 80");
+});
